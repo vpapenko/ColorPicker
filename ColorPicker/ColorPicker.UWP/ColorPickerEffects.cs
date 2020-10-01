f59afee8fa4e38539d37f8b122a11f10530f330e
vpapenko <vpapenko@gmail.com>
2020-10-01 17:26:48 +0300

    GetRendererAssemblies is added for UWP project

---
+++ ColorPicker/ColorPicker.UWP/ColorPickerEffects.cs
@@ -0,0 +1,13 @@
+﻿using ColorPicker.UWP.Effects;
+using System.Reflection;
+
+namespace ColorPicker.UWP
+{
+    public static class ColorPickerEffects
+    {
+        public static Assembly[] GetRendererAssemblies()
+        {
+            return new Assembly[] { typeof(ColorPickerTouchEffectUWP).GetTypeInfo().Assembly };
+        }
+    }
+}
